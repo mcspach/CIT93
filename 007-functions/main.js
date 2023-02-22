495a2b5ade3c71701d276815da115e5aec70d5ca
Matthew Spach <matthews@anyfans.com>
2023-02-21 21:50:23 -0800

    fun with functions

---
+++ 007-functions/main.js
@@ -0,0 +1,14 @@
+// functions are fun
+
+function syntaxExample () {
+    //code to run
+}
+
+function doMath (x, y) {
+    let result = x * y;
+    console.log(`${result}, ${x / y}, ${x + y}, ${x - y}`);
+    return result;
+}
+
+//call or invoke 
+doMath(10, 4);
