903b1ace4045eed7ad79281f5930e1935df60b37
Matthew Spach <matthews@anyfans.com>
2023-02-21 19:28:46 -0800

    week 4

---
+++ 005-data-types/main.js
@@ -0,0 +1,9 @@
+let veryable = "string cheese";
+let numero = 109;
+let truthy = true;
+console.log(veryable, numero, truthy);
+console.log(typeof(veryable), typeof(numero), typeof(truthy));
+
+let allOfThem = { string: 'string cheese', numero: 109, truthy: true };
+console.log(allOfThem.string, allOfThem.numero, allOfThem.truthy);
+console.log(typeof(allOfThem.string), typeof(allOfThem.numero), typeof(allOfThem.truthy));
