8e379b90cf11121b89e2960ee49a47b298e2aab0
Matthew Spach <matthews@anyfans.com>
2023-02-21 21:23:01 -0800

    more on variables

---
+++ 006-variables/main.js
@@ -0,0 +1,20 @@
+let myName = "Matthew";
+let age = 109;
+let happiness = true;
+
+console.log(myName, age, happiness);
+
+const myButton = document.getElementsByTagName('button')[0];
+console.log(myButton);
+
+const log = () => {
+    document.getElementById('myName').innerHTML = document.getElementById('nameInput').value;
+    document.getElementById('age').innerHTML = document.getElementById('ageInput').value;
+    let select = document.getElementById('happinessSelect');
+    document.getElementById('happiness').innerHTML = select.options[select.selectedIndex].value;
+    if ( document.getElementById('age').innerHTML == '31' ) {
+        document.getElementById('age').innerHTML = 'Happy Birthday!!!!!!!!!!!!!!!!!!!!!!!!!!!!!';
+    }
+}
+myButton.addEventListener(`click`, log);
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Week 6 Video 1 My Thoughts
+Everyone I know prefers the integrated terminal, and for some reason I still favor the classic terminal like Todd. I also may have missed where you specfified what was specific about the commit messages you are looking for.
+
