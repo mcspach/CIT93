110f9f16415fe9034d81ab66f12b1ae1ca77caee
Matthew Spach <matthews@anyfans.com>
2023-02-23 09:40:28 -0800

    writing the mileage function

---
+++ 008-build-something/README.md
@@ -0,0 +1,7 @@
+# Using Devtools
+
+I had completely forgotten about marking breakpoints indside the sources panel and stepping through line by line. This is a huge help and I think it will make me a much better programmer!
+
+I had also forgotten the difference between null and undefined, so this lesson was a good reason to go revisit that concept in the documentation.
+
+Love that you can hover a variable and see the value!
--- 008-build-something/main.js
+++ 008-build-something/main.js
@@ -1,9 +1,17 @@
 //Updating the DOM
-text = 'This text was inserted by main.js';
-const updateDOM = (text) => {
-    const divEl = document.getElementById('output');
-    const h2 = document.createElement('h2');
-    h2.textContent = text;
-    divEl.appendChild(h2);
+
+const updateDOM = (input) => {
+    const divEl = document.getElementById('output')
+    const h2 = document.createElement('h2')
+    h2.textContent = `Total cost per mile for my car is ${input}`
+    divEl.appendChild(h2)
 }
-updateDOM(text);
+
+// I dont understand why we are multiplying the mpg  by the price, what is that return value representing?
+// I changed it to deviding the price by mpg to show cost per mile.
+const mileage = (miles, gallons = 10, price = 4.0) => updateDOM(price/(miles/gallons))
+
+const myMPG = mileage(300, 10, 4.50)
+// updateDOM(100)
+
+
