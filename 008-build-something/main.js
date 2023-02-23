00faf7cc294891657dd57702fed1995966ee7cc0
Matthew Spach <matthews@anyfans.com>
2023-02-23 08:59:53 -0800

    Updating the DOM

--- 008-build-something/main.js
+++ 008-build-something/main.js
@@ -1,9 +1,9 @@
 //Updating the DOM
 text = 'This text was inserted by main.js';
 const updateDOM = (text) => {
-    const divEl = document.querySelector('#output');
-    const p = document.createElement('p');
-    p.textContent = text;
-    divEl.appendChild(p);
+    const divEl = document.getElementById('output');
+    const h2 = document.createElement('h2');
+    h2.textContent = text;
+    divEl.appendChild(h2);
 }
 updateDOM(text);
