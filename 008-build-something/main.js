38f22e3c342af0c8e7c03da9a6af907ac240e4ca
Matthew Spach <matthews@anyfans.com>
2023-03-01 21:57:38 -0800

    fixing errors

--- 008-build-something/main.js
+++ 008-build-something/main.js
@@ -1,20 +1,30 @@
 //Updating the DOM
-
-// let arr = ['mott', 10];
+let myArr = []
 
 const updateDOM = (input) => {
     const divEl = document.getElementById('output')
     const h2 = document.createElement('h2')
-    h2.textContent = `Total cost per mile for my car is ${input}`
+    const h22 = document.createElement('h2')
+    if (typeof input === 'object') {
+        h2.textContent = `Total miles per gallon for my car is ${input[0]}. Total trip cost is ${input[1]}.`
+    } else {
+        h22.textContent = input
+    }
+    
     divEl.appendChild(h2)
 }
 
 // I dont understand why we are multiplying the mpg  by the price, what is that return value representing?
 // Nevermind, you answered that ^
 // I changed it to deviding the price by mpg to show cost per mile.
-const mileage = (miles, gallons = 10, price = 4.0) => updateDOM(price/(miles/gallons))
+const trackMPGCost = (miles, gallons, price = 4.0) => {
+    const MPG  = miles / gallons
+    const tripCost = gallons * price
+    updateDOM(`MPG: ${MPG}, Total Cost: ${tripCost}`)
+    myArr.push(MPG, tripCost)
+}
 
-const myMPG = mileage(300, 10, 4.50)
-// updateDOM(100)
+const myMPG = trackMPGCost(300, 10)
+updateDOM(myArr)
 
 
