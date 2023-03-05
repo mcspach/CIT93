45b0c34b91a03ccedbd202d3ddba5c84e4ea5799
Matthew Spach <matthews@anyfans.com>
2023-03-04 21:06:53 -0800

    the this variable

--- 017-intro-to-objects/main.js
+++ 017-intro-to-objects/main.js
@@ -3,15 +3,16 @@ const myArr = []
 const myObj = {
    name: "Morty",
    age: 75,
-   hobbies: ["coding", "running", "chess", "photography"],
+   hobbies: ["joking", "running", "chess", "photography"],
    computer: {
     make: 'macbook',
     OS: 'macOS'
    },
    myGreeting: function(person) {
-    return `What is Up, ${person}!`
+    console.log(this)
+    return `${this.name} who loves ${this.hobbies[0]} says: What is Up, ${person}!`
    }
 }
 
-const greeting = myObj.myGreeting('Ponchie')
+const greeting = myObj.myGreeting('Hoss')
 console.log(greeting)
