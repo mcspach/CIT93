3354887fe7248e88cffbf636f5e243bc187c99f7
Matthew Spach <matthews@anyfans.com>
2023-03-04 22:17:11 -0800

    += objects

--- 017-intro-to-objects/main.js
+++ 017-intro-to-objects/main.js
@@ -3,6 +3,7 @@ const myArr = []
 const myObj = {
    name: "Morty",
    age: 75,
+   likes: 0,
    hobbies: ["joking", "running", "chess", "photography"],
    computer: {
     make: 'macbook',
@@ -11,33 +12,57 @@ const myObj = {
    myGreeting: function(person) {
     console.log(this)
     return `${this.name} who loves ${this.hobbies[0]} says: What is Up, ${person}!`
+   },
+   increaseLikes: function() {
+    this.likes++
    }
 }
+
+const brandNewObj = myObj
+brandNewObj.increaseLikes()
+// brandNewObj.name = "Bingo" - passed by referewnce because its an object.
 
 const greeting = myObj.myGreeting('Hoss')
 console.log(greeting)
 
-const sideKicks = {
-    batman: "Robin",
-    greenHornet: "Kato",
-    mario: "Luigi"
+console.log(myObj.computer.make.length)
+
+let monkey = "monkey"
+let monkey2 = monkey
+console.log(monkey2) 
+console.log(monkey)
+monkey2 = "Not monkey"
+console.log(monkey2) // changed
+console.log(monkey) // not changed
+monkey = "Still monkey"
+console.log(monkey2) // changed?
+console.log(monkey) // changed
+
+for (key in myObj) {
+    console.log(`${key} and value ${myObj[key]}`)
 }
 
-const superHero = {
-    level: 1,
-    name: "BatMan",
-    secretIdentity: "Bruce Wayne",
-    vehicle: "Batmobile",
-    sideKick: sideKicks.batman,
-    priveledgedWhiteMale: true,
-    levelUp: function (level) {
-        console.log(`Previous level: ${this.level}`)
-        this.level++
-    }
-}
+// const sideKicks = {
+//     batman: "Robin",
+//     greenHornet: "Kato",
+//     mario: "Luigi"
+// }
 
-console.log(superHero.level)
-superHero.levelUp(superHero.level)
-console.log(`${superHero.name} has a sisekick named ${superHero.sideKick}. and he is currently at level ${superHero.level}`)
-superHero.levelUp(superHero.level)
-console.log(`${superHero.name} has a sisekick named ${superHero.sideKick}. and he is currently at level ${superHero.level}`)
+// const superHero = {
+//     level: 1,
+//     name: "BatMan",
+//     secretIdentity: "Bruce Wayne",
+//     vehicle: "Batmobile",
+//     sideKick: sideKicks.batman,
+//     priveledgedWhiteMale: true,
+//     levelUp: function (level) {
+//         console.log(`Previous level: ${this.level}`)
+//         this.level++
+//     }
+// }
+
+// console.log(superHero.level)
+// superHero.levelUp(superHero.level)
+// console.log(`${superHero.name} has a sisekick named ${superHero.sideKick}. and he is currently at level ${superHero.level}`)
+// superHero.levelUp(superHero.level)
+// console.log(`${superHero.name} has a sisekick named ${superHero.sideKick}. and he is currently at level ${superHero.level}`)
