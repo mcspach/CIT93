1d47c3f301ee2f67397c4ce5fe5d380a7f89fb2f
Matthew Spach <matthews@anyfans.com>
2023-05-04 10:49:03 -0700

    my function

--- 100-my-app/README.md
+++ 100-my-app/README.md
@@ -1,3 +1,19 @@
 # App for Eating
 
 This application will decide whether or not to eat, and what Macro nutrient to induilge in. It first decides whether or not we are within eating hours, then decides whether or not we are hungry. If those two guards are passed, then the inner loop recommends different options based on which nutrients have been consumed at higher levels that day.
+
+Week 11 Instructions:
+
+FUNCTION(s)
+
+[X] Implement at least 1 function where you pass arguments into the function as parameters that were global variables last week and have a few global variables as possible. 
+[X] The function should return an obj which contains the data passed into the function and the decision as a string or array. 
+[X] Make sure to call the function with different inputs to test that you decision logic is working as you would except. 
+Once you have working code or you want to see a video of me discussion my code for this step, commit with message "my function"
+
+FORM
+
+In your HTML create the form for getting input from the user and implement the required attribute (we will do validation next week). 
+In addition to the input, use a checkbox or radio button for the user input.  
+Code an eventlisteners make sure you getting the correct inputs by showing them on the console or updateDOM function, next week you will integrate the functions.
+Once you have working code or you want to see a video of me going over my code for the function and forms, submit with commit "my form" and submit your commit history URL. Make sure you tell me you want the video, because it will NOT be public on my youtube.com channel. 
--- 100-my-app/main.js
+++ 100-my-app/main.js
@@ -1,33 +1,42 @@
-//GLOBALS
-
-let currentDate = new Date();
-const TIME = currentDate.getHours() + ":" + currentDate.getMinutes();
-const HUNGER_LEVEL = 6; //Range 1-10
-const MACROS = {
+//GLOBAL OBJECT
+const DATA = {
+  macros: {
     fat: 0,
     carbs: 0,
     protein: 0
-};
+  }
+}
 
 function updateDOM(content, id) {
-    const divEl = document.getElementById(id);
-    const p = document.createElement('p');
-    p.innerHTML = content;
-    divEl.appendChild(p);
+  const divEl = document.getElementById(id);
+  const p = document.createElement('p');
+  p.innerHTML = content;
+  divEl.appendChild(p);
+  console.log(divEl.innerHTML);
 }
 
-if (TIME >= "12:00" && TIME <= "21:00") {
-    if (HUNGER_LEVEL > 5) {
-        if (MACROS.fat < MACROS.carbs && MACROS.protein < MACROS.carbs) {
-            updateDOM(`Good job today.You can eat anything you want.`, `output`);
-        } else if (MACROS.fat > MACROS.protein) {
-            updateDOM(`Go eat something high in protein.`, `output`);
+function setTime() {
+  let currentDate = new Date();
+  return currentDate.getHours() + ":" + currentDate.getMinutes();
+}
+
+const decisionMaker = (hungerLevel) => {
+  let time = setTime();
+  if (time >= "12:00" && time <= "21:00") {
+    if (hungerLevel > 5) {
+      if (DATA.macros.fat < DATA.macros.carbs && DATA.macros.protein < DATA.macros.carbs) {
+        updateDOM(`Good job today.You can eat anything you want.`, `output`);
+      } else if (DATA.macros.fat > DATA.macros.protein) {
+        updateDOM(`Go eat something high in protein.`, `output`);
         } else {
-            updateDOM(`Go eat something high in fat.`, `output`);
+        updateDOM(`Go eat something high in fat.`, `output`);
         }
     } else {
-        updateDOM(`Wait until you're hungry to eat`, `output`);
+      updateDOM(`Wait until you're hungry to eat`, `output`);
     }
-} else {
+  } else {
     updateDOM(`It's not munching time. Your interval for eating is 12pm - 9pm.`, `output`);
+  }
 }
+
+decisionMaker(6);
