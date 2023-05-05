bf998710be06f649a7deb072d6ff258c1db853eb
Matthew Spach <matthews@anyfans.com>
2023-05-05 14:33:38 -0700

    my attempt

---
+++ 028-implement-class-into-mileage/README.md
@@ -0,0 +1,9 @@
+# Week 13
+## Classes
+
+Part 1: 
+I think we should create a class called TRIP that inclkudes the three inputs. Then we can save an instance of Trip each time something is submitted or updated. 
+Other than that Im not sure where classes could be used in this app. Because Im not sure that's what you are planning to do Im going to hold off until the next video to actually mess with my code.
+
+
+
---
+++ 028-implement-class-into-mileage/handleForm.js
@@ -0,0 +1,65 @@
+// funny we named our files about the same thing
+import { ERR, TBL_OUTPUT, clearAverages } from './render.js';
+
+function updateDOM(input, id) {
+  const divEl = document.querySelector(id);
+  const p = document.createElement('p');
+  p.textContent = input;
+  divEl.appendChild(p);
+};
+
+function trackMPGandCost(miles, gallons, price) {
+  const MPG = Number((miles / gallons).toFixed(2));
+  const tripCost = Number((gallons * price).toFixed(2));
+  //enable buttons in table
+  const formButtons = TBL_OUTPUT.querySelectorAll('button');
+  formButtons.forEach(function (btn) {
+    btn.disabled = false;
+  });
+  return {
+    miles: miles,
+    gallons: gallons,
+    price: price,
+    MPG: MPG,
+    tripCost: tripCost,
+  };
+}
+
+function calculateAvg(MY_DATA) {
+  clearAverages();
+  const numberOfObj = MY_DATA.length;
+  // let sumMPG = 0;
+  // let sumTripCost = 0;
+  // MY_DATA.forEach(obj => {
+  //   sumMPG += obj.MPG;
+  //   sumTripCost += obj.tripCost;
+  // });
+  const sums = MY_DATA.reduce(function (sum, obj) {
+    return {
+      MPG: sum.MPG + obj.MPG,
+      tripCost: sum.tripCost + obj.tripCost
+    };
+  });
+  const avgMPG = Number((sums.MPG / numberOfObj).toFixed(2));
+  const avgTripCost = Number((sums.tripCost / numberOfObj).toFixed(2));
+  if (avgMPG > 0) updateDOM(`Average MPG is ${avgMPG}`, '#output-mpg');
+  if (avgTripCost > 0) updateDOM(`Average Trip Cost is ${avgTripCost}`, '#output-cost');
+}
+
+function isFormValid(miles, gallons, price) {
+  const errMsg = [];
+  if (miles === 0 || gallons === 0 || price === 0) {
+    errMsg.push('Make sure your input value greater than 0!, Try Again');
+  }
+  if (price > 1000) {
+    errMsg.push('Really!? I think this is in error... Try again');
+  }
+  if (errMsg.length > 0) {
+    ERR.textContent = errMsg;
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export { isFormValid, calculateAvg, trackMPGandCost };
