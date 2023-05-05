d49508820b5acbf52ad9f55f22a9a2df0dc33fbc
Matthew Spach <matthews@anyfans.com>
2023-05-05 12:06:49 -0700

    student subclass

--- 027-intro-to-oop/README.md
+++ 027-intro-to-oop/README.md
@@ -1,5 +1,4 @@
 Week 13
 OOP
 
-I have seen OOP used before in JS a bit, and also in Ruby on Rails. 
-The syntax makes alot of sense. 
+Prototype chain is awesome! THis week of lessons is fun so far!
--- 027-intro-to-oop/main.js
+++ 027-intro-to-oop/main.js
@@ -9,7 +9,17 @@ class Person {
   }
 }
 
-const rio = new Person("Rio", "Waller", 39);
+class Student extends Person {
+  constructor(firstName, lastName, age, classList) {
+    super(firstName, lastName, age);
+    this.classList = classList || [];
+  }
+  greet() {
+    console.log(`Hello, my name is ${this.firstName}. I am ${this.age} years old. I am taking ${this.classList.join(', ')} this semester.`);
+  }
+}
+
+const rio = new Student("Rio", "Waller", 39, ["Math", "Science", "English"]);
 rio.greet();
 const matthew = new Person();
 matthew.greet();
