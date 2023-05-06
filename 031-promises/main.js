6e5e07a51830316778728d241ba7bc3f565393a9
Matthew Spach <matthews@anyfans.com>
2023-05-06 14:50:17 -0700

    more promise methods

--- 031-promises/main.js
+++ 031-promises/main.js
@@ -28,4 +28,34 @@ function onError(err) {
 
 getWeather()
   .then(getWeatherIcon)
-  .then(onSuccess, onError);
+  .then(onSuccess)
+  .catch(onError);
+
+
+function fun1() {
+  return new Promise((resolve, reject) =>
+    setTimeout(() => resolve('404'), 100));
+}
+
+function fun2() {
+  return new Promise((resolve, reject) =>
+    setTimeout(() => resolve('🥸'), 100));
+}
+
+function onSuccess(data) {
+  console.log(`Success: ${data}`);
+}
+
+function onError(err) {
+  console.log(`Error: ${err}`);
+}
+
+function onFinally() {
+  console.log(`Finally`);
+}
+
+fun1()
+  .then(fun2, onError)
+  .then(onSuccess)
+  .catch(onError)
+  .finally(onFinally);
