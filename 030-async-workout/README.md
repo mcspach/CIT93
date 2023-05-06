2411ff43241f992eb4cea7a31ec2de02be4798b8
Matthew Spach <matthews@anyfans.com>
2023-05-06 15:28:29 -0700

    promises - rios solution

--- 030-async-workout/README.md
+++ 030-async-workout/README.md
@@ -3,3 +3,6 @@ Asynchronous Workout App
 
 PROMISES ASSIGNMENT:
 -I added an extra funtion which clears message that the exercise is finished. The entire exercise is wrapped in a Promise, then the dom is cleared only after the exercise is complete. I dont know if this is what you were looking for exactly, but it is a working use of a Promise.
+
+
+- I had working code but I coded along with you anyway to have both versions.
--- 030-async-workout/main.js
+++ 030-async-workout/main.js
@@ -1,69 +1,40 @@
 const formEl = document.getElementById('form');
 const output = document.getElementById('output');
-let counterDiv;
+// let counterDiv;
 
-const updateDOM = (exercise, reps, time) => {
-  return new Promise((res, rej) => {
-    counterDiv = document.createElement('div');
-    counterDiv.classList.add('counter');
-    counterDiv.innerHTML = `Do ${exercise} for ${time} minutes. Your goal is ${reps} reps.`;
-    output.insertBefore(counterDiv, output.firstChild);
-    // for testing purposes, using seconds rather than minutes.
-    // let minutes = time * 1000 * 60;
-    let minutes = time * 1000;
-    setTimeout(() => {
-      const li = document.createElement('li');
-      li.innerHTML = `
-    <strong>${exercise}</strong>
-    <span>Reps: ${reps}</span>
-    <span>Time: ${time}</span>
-  `;
-      // output.innerHTML = '';
-      output.appendChild(li);
-      counterDiv.innerHTML = `${exercise} completed.`;
-    }, minutes);
-    res(3000);
-  });
-};
+// const updateDOM = (exercise, reps, time) => {
+//   return new Promise((res, rej) => {
+//     counterDiv = document.createElement('div');
+//     counterDiv.classList.add('counter');
+//     counterDiv.innerHTML = `Do ${exercise} for ${time} minutes. Your goal is ${reps} reps.`;
+//     output.insertBefore(counterDiv, output.firstChild);
+//     // for testing purposes, using seconds rather than minutes.
+//     // let minutes = time * 1000 * 60;
+//     let minutes = time * 1000;
+//     setTimeout(() => {
+//       const li = document.createElement('li');
+//       li.innerHTML = `
+//     <strong>${exercise}</strong>
+//     <span>Reps: ${reps}</span>
+//     <span>Time: ${time}</span>
+//   `;
+//       // output.innerHTML = '';
+//       output.appendChild(li);
+//       counterDiv.innerHTML = `${exercise} completed.`;
+//     }, minutes);
+//     res(3000);
+//   });
+// };
 
-function clearCounter(ms) {
-  // clear the counterDiv after a few seconds.
-  setTimeout(() => {
-    counterDiv.remove();
-  }, ms);
-}
-
-function onError(err) {
-  console.log(err);
-}
-
-formEl.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const exercise = e.target.exercise.value;
-  const reps = parseInt(e.target.reps.value);
-  const time = parseFloat(e.target.time.value);
-  formEl.reset();
-  updateDOM(exercise, reps, time)
-    .then(clearCounter)
-    .catch(onError);
-});
-
-
-
-
-//EVERYTHING BELOW HERE IS RIO'S CODE
-
-// function updateDOM(message, el) {
-//   const newEl = document.createElement(el);
-//   newEl.textContent = message;
-//   output.appendChild(newEl);
+// function clearCounter(ms) {
+//   // clear the counterDiv after a few seconds.
+//   setTimeout(() => {
+//     counterDiv.remove();
+//   }, ms);
 // }
 
-// function startWorkout(exercise, reps, time, fn) {
-//   fn(`Start ${exercise} <> Goal is ${reps} reps.`, 'p');
-//   setTimeout(() => {
-//     fn(`${exercise} completed.`, 'p');
-//   }, time * 1000);
+// function onError(err) {
+//   console.log(err);
 // }
 
 // formEl.addEventListener('submit', (e) => {
@@ -71,6 +42,43 @@ formEl.addEventListener('submit', (e) => {
 //   const exercise = e.target.exercise.value;
 //   const reps = parseInt(e.target.reps.value);
 //   const time = parseFloat(e.target.time.value);
-//   startWorkout(exercise, reps, time, updateDOM);
 //   formEl.reset();
+//   updateDOM(exercise, reps, time)
+//     .then(clearCounter)
+//     .catch(onError);
 // });
+
+
+
+
+//EVERYTHING BELOW HERE IS RIO'S CODE
+
+function updateDOM(message, el) {
+  const newEl = document.createElement(el);
+  newEl.textContent = message;
+  output.appendChild(newEl);
+}
+
+function startWorkout(exercise, reps, time, fn) {
+  fn(`Start ${exercise} <> Goal is ${reps} reps.`, 'p');
+  return new Promise(function (resolve, reject) {
+    {
+      setTimeout(() => {
+        reject(fn(`${exercise} completed.`, 'p'));
+      }, time * 1000);
+    }
+  });
+};
+
+function onError(err) {
+  console.log(`Error: ${err}`);
+}
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const exercise = e.target.exercise.value;
+  const reps = parseInt(e.target.reps.value);
+  const time = parseFloat(e.target.time.value);
+  startWorkout(exercise, reps, time, updateDOM).then().catch(onError);
+  formEl.reset();
+});
