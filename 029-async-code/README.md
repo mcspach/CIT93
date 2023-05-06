40754f74c31350e4971b56e75c8ed0f5475d8311
Matthew Spach <matthews@anyfans.com>
2023-05-05 17:53:00 -0700

    sync

---
+++ 029-async-code/README.md
@@ -0,0 +1,3 @@
+# Week 14
+Asynchronous Code
+
---
+++ 029-async-code/main.js
@@ -0,0 +1,8 @@
+let pizza;
+function orderPizza() {
+  console.log('Order pizza');
+  setTimeout(() => { pizza = '🍕'; }, 2000);
+  console.log('Pizza was ordered');
+}
+orderPizza();
+console.log(`Eat ${pizza}`);
