43c039bb2c947b9904c9f38bd63fd0f864021fad
Matthew Spach <matthews@anyfans.com>
2023-05-06 11:43:09 -0700

    workout coding challenge - coded along

--- 030-async-workout/README.md
+++ 030-async-workout/README.md
@@ -3,3 +3,5 @@ Asynchronous Workout App
 
 I used a couplke of setTimeout() and I think its working pretty well.
 
+I see now that I'm coding along, my solution used timouts, but not exactly a callback passecd as an argument.
+Having said that, I think my solution is more elegant and functional as a user interface. But since it only uses timeouts rather than callbacks, it doesnt exactly fit the requirements.
--- 030-async-workout/main.js
+++ 030-async-workout/main.js
@@ -1,30 +1,42 @@
 const formEl = document.getElementById('form');
 const output = document.getElementById('output');
 
-const updateDOM = (exercise, reps, time) => {
-  counterDiv = document.createElement('div');
-  counterDiv.classList.add('counter');
-  counterDiv.innerHTML = `Do ${exercise} for ${time} minutes. Your goal is ${reps} reps.`;
-  output.insertBefore(counterDiv, output.firstChild);
-  // for testing purposes, using seconds rather than minutes.
-  // let minutes = time * 1000 * 60;
-  let minutes = time * 1000;
-  setTimeout(() => {
-    const li = document.createElement('li');
-    li.innerHTML = `
-    <strong>${exercise}</strong>
-    <span>Reps: ${reps}</span>
-    <span>Time: ${time}</span>
-  `;
-    // output.innerHTML = '';
-    output.appendChild(li);
-    counterDiv.innerHTML = `${exercise} completed.`;
-    // clear the counterDiv after a few seconds.
-    setTimeout(() => {
-      counterDiv.remove();
-    }, 3000);
-  }, minutes);
+// const updateDOM = (exercise, reps, time) => {
+//   counterDiv = document.createElement('div');
+//   counterDiv.classList.add('counter');
+//   counterDiv.innerHTML = `Do ${exercise} for ${time} minutes. Your goal is ${reps} reps.`;
+//   output.insertBefore(counterDiv, output.firstChild);
+//   // for testing purposes, using seconds rather than minutes.
+//   // let minutes = time * 1000 * 60;
+//   let minutes = time * 1000;
+//   setTimeout(() => {
+//     const li = document.createElement('li');
+//     li.innerHTML = `
+//     <strong>${exercise}</strong>
+//     <span>Reps: ${reps}</span>
+//     <span>Time: ${time}</span>
+//   `;
+//     // output.innerHTML = '';
+//     output.appendChild(li);
+//     counterDiv.innerHTML = `${exercise} completed.`;
+//     // clear the counterDiv after a few seconds.
+//     setTimeout(() => {
+//       counterDiv.remove();
+//     }, 3000);
+//   }, minutes);
+// }
 
+function updateDOM(message, el) {
+  const newEl = document.createElement(el);
+  newEl.textContent = message;
+  output.appendChild(newEl);
+}
+
+function startWorkout(exercise, reps, time, fn) {
+  fn(`Start ${exercise} <> Goal is ${reps} reps.`, 'p');
+  setTimeout(() => {
+    fn(`${exercise} completed.`, 'p');
+  }, time * 1000);
 }
 
 formEl.addEventListener('submit', (e) => {
@@ -32,6 +44,6 @@ formEl.addEventListener('submit', (e) => {
   const exercise = e.target.exercise.value;
   const reps = parseInt(e.target.reps.value);
   const time = parseFloat(e.target.time.value);
-  updateDOM(exercise, reps, time);
+  startWorkout(exercise, reps, time, updateDOM);
   formEl.reset();
 });
