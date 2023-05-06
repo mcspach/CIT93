990ad2a5803608d6808abeb2e25ceb7385d2b8ab
Matthew Spach <matthews@anyfans.com>
2023-05-06 14:56:19 -0700

    real world example

--- 031-promises/main.js
+++ 031-promises/main.js
@@ -59,3 +59,23 @@ fun1()
   .then(onSuccess)
   .catch(onError)
   .finally(onFinally);
+
+function fetchData() {
+  return new Promise(function (resolve, reject) {
+    fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
+      .then(response => response.json())
+      .then(data => console.log(data.properties.periods[1].shortForecast));
+  });
+}
+
+function displayData(weather) {
+  console.log(weather);
+}
+
+function onError(err) {
+  console.log(`Error: ${err}`);
+}
+
+fetchData()
+  .then(displayData)
+  .catch(onError);
