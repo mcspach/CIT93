b73e06849e18d49cc3082263f806925afc12eef2
Matthew Spach <matthews@anyfans.com>
2023-05-07 13:10:49 -0700

    try-catch

--- 032-async-await/main.js
+++ 032-async-await/main.js
@@ -1,25 +1,54 @@
+// function getData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(46 * 2);
+//     }, 1);
+//   });
+// }
+
+// // the following methods do the exact same thing.
+
+// async function start() {
+//   const data = await fetch('https://api.weather.gov/points/36.746841,-119.772591');
+//   const result = await data.json();
+//   console.log(result);
+// }
+
+// async function start2() {
+//   fetch('https://api.weather.gov/points/36.746841,-119.772591')
+//     .then(data => data.json())
+//     .then(result => console.log(result));
+// }
+
+
+// start();
+// start2();
+
+// TRY / CATCH
+
 function getData() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(46 * 2);
+      resolve('OOops');
     }, 1);
   });
 }
 
-// the following methods do the exact same thing.
-
 async function start() {
-  const data = await fetch('https://api.weather.gov/points/36.746841,-119.772591');
-  const result = await data.json();
-  console.log(result);
+  try {
+    const result = await getData();
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function start2() {
-  fetch('https://api.weather.gov/points/36.746841,-119.772591')
-    .then(data => data.json())
-    .then(result => console.log(result));
+  const result = await getData()
+    .catch(error => console.log(error));
+  console.log(result);
 }
+// Above does trigger the catch IF the method rejects, but the rest of the code still runs. 
 
 
 start();
-start2();
