c90ca4c2eff09578a706fe8256290a82da8cccdd
Matthew Spach <matthews@anyfans.com>
2023-05-13 20:00:59 -0700

    API keys

--- 034-fetch-api/README.md
+++ 034-fetch-api/README.md
@@ -2,3 +2,4 @@
 
 First questions: YES! I always go and run the code in the browser and debug if it isnt working.
 
+I decided not to go get the API key because you said it was optional.
--- 034-fetch-api/main.js
+++ 034-fetch-api/main.js
@@ -1,5 +1,6 @@
 // const url = "http://worldtimeapi.org/api/timezone/America/Los_Angeles";
 const url = "https:/api.thenewsapi.com/v1/news/top?";
+// I decided not to go get the API key because you said it was optional.
 
 async function getData() {
   const response = await fetch(url);
